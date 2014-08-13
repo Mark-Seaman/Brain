4f2a02c20851689c180bad573741569a45d79845
Mark Seaman <seaman@seaman-hammer>
2014-08-13 11:10:46 -0600

    Update pages

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,13 +3,15 @@
 
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',  
-    url(r'^$', 'app.views.home', name='/'),
-    url(r'^user$', 'app.views.user', name='user'),
-    url(r'^test$', 'app.views.test_view', name='test'),
-    url(r'^no_access$', 'app.views.no_access'),
+urlpatterns = patterns(
+    '',  
 
-    (r'^login', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),  
+    url(r'^$',              'app.views.home', name='/'),
+    url(r'^user$',          'app.views.user', name='user'),
+    url(r'^test$',          'app.views.test_view', name='test'),
+    url(r'^no_access$',     'app.views.no_access'),
+    
+    (r'^login', 'django.contrib.auth.views.login', {'template_name':'login.html'}),  
     (r'^logout', 'app.views.logout_view'),
 
     url(r'^doc', include('doc.doc_urls')),
